240f2c25206fea3976f9dd057e4867eb71a2364f
Ghiyatsni <107105791+Ghiyatsni@users.noreply.github.com>
2024-01-11 02:07:30 +0700

    Delete Assesment3 dan tubes directory

--- Assesment3 dan tubes/app/src/main/java/com/example/loginregisterrelative/User.kt
+++
@@ -1,3 +0,0 @@
-package com.example.loginregisterrelative
-
-data class User(var name : String, var lastmessage : String, var lastMsgTime : String,var phoneNo : String, var country : String, var imageId : Int )
